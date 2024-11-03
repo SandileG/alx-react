1daafea0bf4e60be445255a740ea4a77d1632cb3
SandileG <sandilegraham@gmail.com>
2024-11-03 01:57:35 +0000

    task_1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+// Add elements to the DOM
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button id="clickButton">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+// Initialize click count
+let count = 0;
+
+// Function to update click count
+function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+}
+
+// Debounce the updateCounter function and bind it to the button click
+$('#clickButton').on('click', _.debounce(updateCounter, 500));
