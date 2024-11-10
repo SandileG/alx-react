7cfd6b21de7048d38f0d24287f90157fea0d0afb
SandileG <sandilegraham>
2024-11-10 21:56:54 +0200

    redo

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './js/dashboard_main.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    }
+  mode: 'production',  // Set the mode to production
+  entry: './js/dashboard_main.js',  // Entry point for your app
+  output: {
+    filename: 'bundle.js',  // Output file name
+    path: path.resolve(__dirname, 'public'),  // Output path
+  },
 };
--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,25 +1,23 @@
 import $ from 'jquery';
 import _ from 'lodash';
-import '../css/main';
 
-// Add logo element
-$('body').prepend('<div id="logo"></div>');
+$(document).ready(function () {
+  // Add paragraphs and button to the body
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 
-// Add elements to the DOM
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button id="clickButton">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+  // Initialize count variable
+  let count = 0;
 
-// Initialize click count
-let count = 0;
-
-// Function to update click count
-function updateCounter() {
-    count += 1;
+  // Define updateCounter function
+  function updateCounter() {
+    count++;
     $('#count').text(`${count} clicks on the button`);
-}
+  }
 
-// Debounce the updateCounter function and bind it to the button click
-$('#clickButton').on('click', _.debounce(updateCounter, 500));
+  // Bind debounce to updateCounter function on button click
+  $('button').on('click', _.debounce(updateCounter, 300));
+});
