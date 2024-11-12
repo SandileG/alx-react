c447af1c8df89495ba04fe4f39fbcedcdcc8a557
SandileG <sandilegraham>
2024-11-12 12:25:27 +0200

    done

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,48 +1,48 @@
-const path = require("path");
+t path = require("path");
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   plugins: [
-		new HTMLWebpackPlugin({
-			filename: './index.html',
-		}),
-		new CleanWebpackPlugin(),
-	],
-	devtool: 'inline-source-map',
-	mode: 'development',
-	entry: {
-		header: {
-			import: './modules/header/header.js',
-			dependOn: 'shared',
-		},
-		body: {
-			import: './modules/body/body.js',
-			dependOn: 'shared',
-		},
-		footer: {
-			import: './modules/footer/footer.js',
-			dependOn: 'shared',
-		},
-		shared: 'jquery',
-	},
+    new HTMLWebpackPlugin({
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devtool: 'inline-source-map',
+  mode: 'development',
+  entry: {
+    header: {
+      import: './modules/header/header.js',
+      dependOn: 'shared',
+    },
+    body: {
+      import: './modules/body/body.js',
+      dependOn: 'shared',
+    },
+    footer: {
+      import: './modules/footer/footer.js',
+      dependOn: 'shared',
+    },
+    shared: 'jquery',
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: '[name].bundle.js',
   },
   optimization: {
-		splitChunks: {
-			chunks: 'all',
-		},
-	},
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
   devServer: {
     static: path.join(__dirname, './public'),
     open: true,
     port: 8564,
   },
   performance: {
-		maxAssetSize: 1000000,
-	},
+    maxAssetSize: 1000000,
+  },
   module: {
     rules: [
       {
@@ -50,17 +50,24 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-				use: [
-					{
-						loader: ['file-loader', 'image-webpack-loader'],
-						options: {
-							bypassOnDebug: true,
-							disable: true,
-						},
-					},
-				],
-			},
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
     ]
   }
 };
+
